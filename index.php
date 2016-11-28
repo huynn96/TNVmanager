b540a4c12a5e21de679d61b6bc8ffcbdd6695e4c
Huy Hoang <nghuyhoang96.isp1415@gmail.com>
2016-11-28 17:00:14 +0700

    update css

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Trang chủ</title>
+        <title>Quản lý người tình nguyện</title>
         <link rel="stylesheet" type="text/css" href="css/index.css">
         <link rel="stylesheet" type="text/css" href="library/css/bootstrap.css">
         <link rel="stylesheet" href="library/jquery-ui-1.12.1/jquery-ui.css">
